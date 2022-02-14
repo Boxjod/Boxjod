54f51221a6cafba43bd9f603f4044e7e7400360c
Boxjod <boxjod@yahoo.com>
2022-02-14 11:59:30 +0800

    love-marry-main
    A Proposed patch

---
+++ CONFIG.js
@@ -0,0 +1,8 @@
+const CONFIGDATA = {
+    titleHeader: "亲爱的舒舒宝子，情人节快乐哦!",
+    descriptionHeader: "这是一起度过的第二个情人节了！你相信我们还有会无数的情人节可以过吗？你相信以后你会嫁给我吗？ ^^",
+    buttonYes: "我非常相信",
+    buttonNo: "我再考虑考虑",
+    titleModar: "嘿嘿，我就知道",
+    descriptionModar: '我想告诉你哦，</br>爱你是我一生最幸福的事，</br>想你是我一生最快乐的事，</br>看到你是我一生最享受的事，</br>爱你想你看你是我一辈子都要做的事，</br>亲爱的舒舒宝子，</br>情人节快乐！ ',
+};
